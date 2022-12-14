bf0c6ddd066d55e9523067f5115930a9792ed7ce
lillyclarke <lillyanneclarke@gmail.com>
2022-12-14 14:49:00 -0800

    app.js/ routes are done

--- server/app.js
+++ server/app.js
@@ -1,18 +1,39 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
-const volleyball = require('volleyball')
-const app = express()
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const volleyball = require('volleyball');
+const { library } = require('webpack');
+const { json } = require('sequelize');
+const app = express();
 
-// static middleware
+//static middleware
 app.use(express.static(path.join(__dirname, '..','public')))
 
 app.use(cors())
 app.use(volleyball)
+
+//route for the students
+app.get('/api/students', (req, res, next) => {
+  res.json(students) //could also use .send instead of json (depends on what form you want it in)
+});
+
+//route for multipleCampuses
+app.get('/api/multipleCampuses', (req, res, next) => {
+  res.json(campuses)
+});
+
+// the + makes sure that it's a integer
+app.get("/api/campuses/:campusId", (req, res, next) => {
+  const campusId = req.params.campusId;
+  const campus = campuses.find((campus) => campus.id === +campusId);
+  campus.students = students.filter(
+    (student) => student.campusId === +campusId
+  );
+  res.json(campus);
+});
 
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = app;
-
